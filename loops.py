39ae9b2939cf8d9e50832ae399764c68589a133f
sadiq70 <69619628+sadiq70@users.noreply.github.com>
2020-10-16 13:53:30 +0530

    adding loops.py

---
+++ loops.py
@@ -0,0 +1,42 @@
+#python program to check whether the given number is in range of 1 and 50.
+""" n = int(input("Enter any number: "))
+if n>=1 and n<=50:
+    print("The number is in between 1 and 50")
+else:
+    print("The number is not in between 1 and 50") """
+
+""" for i in [1,2,3]:   #{}
+    print("hello world")
+    print("Newbegin")
+    print("Bangalore")
+print("Hyderabad") """
+
+""" city = "Hyderabad"
+
+for i in city: # i = H, i = y, i = d
+    print(i) """
+
+""" for i in [1,2,'new',5,6]:
+    print('India') """
+
+""" l = [1,2,3,4,5,6,7,8,9,10]
+for i in l:  #i = , i = 2
+    if i%2 ==0:
+        print(i) """
+
+""" name = 'newbegin'
+# i = b "hello b good morning" "bad morning"
+for i in name:
+    if i == 'b':
+        print("hello b good morning")
+    else:
+        print("bad morning") """
+
+name = 'newbegin'
+vowels = ['a','e','i','o','u'] # in, not in 
+for i in name: #i =n 
+    if i in vowels:
+        print(i)
+
+#python program to print all odd numbers between 1 and 50
+#python program to print multiplies of 3
